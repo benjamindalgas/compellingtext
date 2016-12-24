cd55ded19aad5797375e93a3f8cbbb499a651926
Benjamin Dalgas <benjamindalgas@gmail.com>
2016-12-24 14:11:26 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+function videoplay() {
+    document.getElementById("video").src = "https://www.youtube.com/embed/MX6fjPJCRoI?autoplay=1&showinfo=0&controls=0&&rel=0&start=0&end=8"
+      openNav();
+}
+
+
+function videoplay2() {
+    document.getElementById("video").src = "https://www.youtube.com/embed/d7P9VP6xnp0?autoplay=1&showinfo=0&controls=0&&rel=0&start=0&end=8"
+      openNav();
+}
+
